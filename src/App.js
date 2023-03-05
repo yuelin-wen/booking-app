68c389e413397bdf492b0b6d267ebae1064edc21
yuelin-wen <tyleryuelinwen@gmail.com>
2023-03-05 08:16:08 -0500

    add axios, test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get('https://booking-api-nu.vercel.app/').then((res)=>{
+      console.log(res)
+    })
+  }, []);
+
   return (
     <div className="App">
 
